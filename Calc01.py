a8be452147c5a311e4a66df875486a49f5e18b14
SucreamLee <ganmin1003@gmail.com>
2024-10-04 23:01:02 +0900

    feature_addition

--- Calc01.py
+++ Calc01.py
@@ -4,10 +4,17 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
 
 print("연산 선택.")
 print("1. 덧셈")
 print("2. 뺄셈")
+print("3. 곱셈")
+print("4. 나눗셈")
 print("0. 나가기")
 
 while(True):
@@ -25,6 +32,12 @@ while(True):
   elif choice == '2':
       print(n1, "-", n2, "=", subtract(n1,n2))
 
+  elif choice == '3':
+      print(n1, "*", n2, "=", multiply(n1,n2))
+
+  elif choice == '4':
+      print(n1, "/", n2, "=", divide(n1,n2))
+      
   else:
       print("잘못된 입력입니다.")
   print()
